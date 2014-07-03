99a1a14551bae6744793a80cacc3f5761282273d
Jesus Magana <maganazero5@gmail.com>
2014-07-03 10:52:04 -0700

    Added star sound effect

--- UberJump/StarNode.m
+++ UberJump/StarNode.m
@@ -6,14 +6,34 @@
 //  Copyright (c) 2014 ZeroLinux5. All rights reserved.
 //
 
+@import AVFoundation;
 #import "StarNode.h"
 
+@interface StarNode ()
+{
+    SKAction *_starSound;
+}
+@end
+
 @implementation StarNode
+
+- (id) init
+{
+    if (self = [super init]) {
+        // Sound for when a star is collected
+        _starSound = [SKAction playSoundFileNamed:@"StarPing.wav" waitForCompletion:NO];
+    }
+    
+    return self;
+}
 
 - (BOOL) collisionWithPlayer:(SKNode *)player
 {
     // Boost the player up
     player.physicsBody.velocity = CGVectorMake(player.physicsBody.velocity.dx, 400.0f);
+    
+    // Play sound
+    [self.parent runAction:_starSound];
     
     // Remove this star
     [self removeFromParent];
